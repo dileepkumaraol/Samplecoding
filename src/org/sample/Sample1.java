c8d4b75c1cdba36cb65d86e44bb5f2dd16f86751
dileepkumaraol <dileepkumaraol@gmail.com>
2023-09-26 13:33:25 +0530

    New code updated

--- src/org/sample/Sample1.java
+++ src/org/sample/Sample1.java
@@ -2,6 +2,7 @@ package org.sample;
 
 public class Sample1 {
 public static void main(String[] args) {
-	System.out.println("Hello GitHub");
+	System.out.println("Hello GitHub This is my first code in java");
+	System.out.println("Di Kumar");
 }
 }
