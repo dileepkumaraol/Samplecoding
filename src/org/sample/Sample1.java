4e20baa3881ef91b02c5c0006667155d0d3a71f2
dileepkumaraol <dileepkumaraol@gmail.com>
2023-09-27 20:13:22 +0530

    New code added

--- src/org/sample/Sample1.java
+++ src/org/sample/Sample1.java
@@ -4,5 +4,9 @@ public class Sample1 {
 public static void main(String[] args) {
 	System.out.println("Hello GitHub This is my first code in java");
 	System.out.println("Di Kumar");
+	
+	
+	
+	System.out.println("Online class accessible");
 }
 }
